8ec00fe76633568330ae3f15a7f35e3f8e1f1f07
Daniel Cannon <daniel@iterative.consulting>
2024-05-21 16:02:47 -0600

    CFCHEM-10: Change moa to string

--- src/models/Molecule.ts
+++ src/models/Molecule.ts
@@ -3,7 +3,7 @@ export interface Molecule {
     compound_aliases: string;
     inchi_key: string;
     lcs_id: string;
-    moa: number;
+    moa: string;
     mol_id: string;
     pert_name: string;
     sig_count: number;
