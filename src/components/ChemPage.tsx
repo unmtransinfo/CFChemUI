7c334664a58c0f4e951eb2152f14de2e58d12950
Daniel Cannon <daniel@iterative.consulting>
2024-05-21 18:40:26 -0600

    CFCHEM-14, CFCHEM-15:
    * Export chemical diagram as SVG
    * Export molecule details as CSV
    * Add source indication to rows of molecule details table

--- src/components/ChemPage.tsx
+++ src/components/ChemPage.tsx
@@ -1,10 +1,41 @@
 import {Dispatch, SetStateAction} from "react";
-import {faArrowLeft} from "@fortawesome/free-solid-svg-icons";
+import {faArrowLeft, faDownload} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import MoleculeStructure from "./MoleculeStructure.tsx";
+import {CSVLink} from 'react-csv';
+import {Molecule} from "../models/Molecule.ts";
+import {Button, Link, Tooltip} from "@nextui-org/react";
 
 
 export default function ChemPage(props: { result: Molecule, setChem: Dispatch<SetStateAction<any>> }) {
+    const CSV_DATA = [
+        ['MOA', 'Target', 'Compound Aliases', 'SIG Count', 'LCS ID', 'MOL ID'],
+        [ props.result.moa,
+          props.result.target,
+          props.result.compound_aliases,
+          props.result.sig_count,
+          props.result.lcs_id,
+          props.result.mol_id
+        ]
+    ];
+
+    const DownloadLink = () => (
+        <div className='csv-download-link-wrapper'>
+            <CSVLink data={CSV_DATA} target="_blank" filename="molecule-details.csv">
+                <FontAwesomeIcon icon={faDownload} className="mr-2"/>
+                Export
+            </CSVLink>
+        </div>
+    );
+
+    const DetailsHeading = ({label, source}) => (
+        <th className="font-bold text-nowrap">
+            <Tooltip content={`Data source: ${source}`} color='foreground'>
+                <Link color='secondary' className='cursor-pointer'>{`${label}:`}</Link>
+            </Tooltip>
+        </th>
+    );
+
     return (
         <div id="chem-page" className="relative z-10">
             <button onClick={() => {
@@ -28,37 +59,40 @@ export default function ChemPage(props: { result: Molecule, setChem: Dispatch<Se
                         structure={props.result.smiles}
                         width={350}
                         height={300}
-                        svgMode
+                        svgMode={true}
                     />
                 </section>
             </div>
             <div className="glass-container active p-3 mt-4">
                 <section>
-                    <h3>Molecule Details</h3>
+                    <div className='flex flex-row justify-between items-center'>
+                        <h3>Molecule Details</h3>
+                        <DownloadLink/>
+                    </div>
                     <table className="chem-details-table">
                         <tbody>
                         <tr>
-                            <th className="font-bold">MOA:</th>
+                            <DetailsHeading label='MOA' source='IDG'/>
                             <td>{props.result.moa}</td>
                         </tr>
                         <tr>
-                            <th className="font-bold">Target:</th>
+                            <DetailsHeading label='Target' source='LINCS'/>
                             <td>{props.result.target}</td>
                         </tr>
                         <tr>
-                            <th className="font-bold text-nowrap">Compound&nbsp;Aliases:</th>
+                            <DetailsHeading label='Compound&nbsp;Aliases' source='LINCS'/>
                             <td>{props.result.compound_aliases}</td>
                         </tr>
                         <tr>
-                            <th className="font-bold">SIG Count:</th>
+                            <DetailsHeading label='SIG Count' source='LINCS'/>
                             <td>{props.result.sig_count}</td>
                         </tr>
                         <tr>
-                            <th className="font-bold">LCS ID:</th>
+                            <DetailsHeading label='LCS ID' source='LINCS'/>
                             <td>{props.result.lcs_id}</td>
                         </tr>
                         <tr>
-                            <th className="font-bold">MOL ID:</th>
+                            <DetailsHeading label='MOL ID' source='IDG'/>
                             <td>{props.result.mol_id}</td>
                         </tr>
                         </tbody>
