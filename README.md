e9ab5decf6ee719c4a4292791370c54c709aa903
Jack-42 <johnjack0987@gmail.com>
2025-10-31 10:01:18 -0600

    add heroui theme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CFChemUI
+
 ### (React + TypeScript + Vite)
 
 User interface for [CFChemAPI](https://github.com/unmtransinfo/CFChemAPI)
@@ -13,15 +14,4 @@ npm install
 
 ```shell
 npm run dev
-```
-
-### Front-end assets
-
-_Build:_
-```shell
-npx tailwindcss -i ./src/App.css -o ./dist/App.css
-```
-_Watch:_
-```shell
-npx tailwindcss -i ./src/App.css -o ./dist/App.css --watch
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const port = env.UI_PORT;
   return {
     base: `${assetRoot}/`,
-    plugins: [react()],
+    plugins: [react(), tailwindcss(),],
     server: {
       host: true,
       port: port,
